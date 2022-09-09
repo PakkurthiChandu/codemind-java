715a9714c4d9767922af2dd99964f10262bc92ec
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-09-09 15:11:54 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String a;         a=sc.nextLine();         System.out.println(a);         sc.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a;
+        a=sc.nextLine();
+        System.out.println(a);
+        sc.close();
+    }
+}
