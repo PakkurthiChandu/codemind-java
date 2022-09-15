0790a791e7a452ced6cee8cd73f448fdd2ae61ea
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-09-15 18:09:40 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=(a*(a-3))/2;         System.out.printf("%d",b);         sc.close(); } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=(a*(a-3))/2;
+        System.out.printf("%d",b);
+        sc.close();
+}
+}
