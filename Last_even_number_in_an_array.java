c6735260210a1068f792aea44d01e6407cfc59dd
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-09-15 14:08:20 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,t=0;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2==0)             {                 t=a[i];             }         }         System.out.printf("%d",t);         sc.close();     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,t=0;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2==0)
+            {
+                t=a[i];
+            }
+        }
+        System.out.printf("%d",t);
+        sc.close();
+    }
+}
