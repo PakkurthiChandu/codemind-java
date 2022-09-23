5ff529f28f1b3ce756e46cd2499ebe59278f3e32
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-09-23 14:00:20 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a,b,i;         n=sc.nextInt();         for(i=0;i<n;i++)         {             a=sc.nextInt();             b=sc.nextInt();             System.out.println(a+b);         }
        }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a,b,i;
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            a=sc.nextInt();
+            b=sc.nextInt();
+            System.out.println(a+b);
+        }
+        
+    }
+}
