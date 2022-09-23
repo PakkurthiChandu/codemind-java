f0b74b21a553b0b9ab6d67c32287fdcd0e3f6568
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-09-23 14:21:36 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b=0,f,s,i=2;         a=sc.nextInt();         f=0;         s=1;         System.out.printf("0 1 ");         while(i<a)         {             import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b=0,f,s,i=2;         a=sc.nextInt();         f=0;         s=1;         System.out.printf("0 1 ");         while(i<=a)         {             i++;             b=f+s;             f=s;             s=b;             System.out.printf("%d ",b);         }     } }             b=f+s;             f=s;             s=b;             System.out.printf("%d ",b);         }     } }

---
+++ fibonacci.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b=0,f,s,i=2;
+        a=sc.nextInt();
+        f=0;
+        s=1;
+        System.out.printf("0 1 ");
+        while(i<a)
+        {
+            import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b=0,f,s,i=2;
+        a=sc.nextInt();
+        f=0;
+        s=1;
+        System.out.printf("0 1 ");
+        while(i<=a)
+        {
+            i++;
+            b=f+s;
+            f=s;
+            s=b;
+            System.out.printf("%d ",b);
+        }
+    }
+}
+            b=f+s;
+            f=s;
+            s=b;
+            System.out.printf("%d ",b);
+        }
+    }
+}
