3bb62bfde1702f783ec21545483bb4ddb8621d74
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-09-30 15:37:37 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         int sum=0;         for(int i=0;i<a.length();i++)         {             if(a.charAt(i)>='0' && a.charAt(i)<='9')             {                 sum+=(int)a.charAt(i)-'0';             }         }         System.out.println(sum);     } }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        int sum=0;
+        for(int i=0;i<a.length();i++)
+        {
+            if(a.charAt(i)>='0' && a.charAt(i)<='9')
+            {
+                sum+=(int)a.charAt(i)-'0';
+            }
+        }
+        System.out.println(sum);
+    }
+}
