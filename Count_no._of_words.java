a73ac96d49cc157793aa4122891307310ab391be
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-10-14 22:37:33 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String a;         a=sc.nextLine();         String[] b=a.split(" ");         System.out.println(b.length);     } }

---
+++ Count_no._of_words.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a;
+        a=sc.nextLine();
+        String[] b=a.split(" ");
+        System.out.println(b.length);
+    }
+}
