53d923469efe4c3ff903b9235f979a81991d9d2d
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-10-22 14:34:34 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt(),i,j;         for(i=1;i<=a;i++)         {             for(j=1;j<=a;j++)             {                 System.out.printf("%d ",i);             }             System.out.println();         }     } }

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),i,j;
+        for(i=1;i<=a;i++)
+        {
+            for(j=1;j<=a;j++)
+            {
+                System.out.printf("%d ",i);
+            }
+            System.out.println();
+        }
+    }
+}
