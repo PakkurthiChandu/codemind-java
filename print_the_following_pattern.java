643dda0fb255453aca0355aa483787dc55b7df77
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-10-22 14:48:38 +0530

    import java.util.*; class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         for(int i=1;i<=a;i++)         {             for(int j=1;j<=a;j++)             {                 if(j==i)                 {                     System.out.printf("0");                 }                 else                 System.out.printf("x");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,16 +1,22 @@
 import java.util.*;
-class Codemind{
- public static void main(String args[])
- {
-     Scanner sc=new Scanner(System.in);
-     int a=sc.nextInt();
-     for(int i=1;i<=a;i++)
-     {
-         for(int j=1;j<=a;j++)
-         {
-             System.out.printf("%c ",i+64);
-         }
-         System.out.println();
-     }
- }
+class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=1;j<=a;j++)
+            {
+                if(j==i)
+                {
+                    System.out.printf("0");
+                }
+                else
+                System.out.printf("x");
+            }
+            System.out.println();
+        }
+    }
 }
