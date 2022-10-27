d825e52c5ec9279d0470d35bbb3337656b3e333a
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-10-27 11:20:40 +0530

    import java.util.*; class Codemind{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         int f=0,s=1,m;         while(s<a)         {             m=f+s;             f=s;             s=m;         }         if((s-a)==(a-f))         {             System.out.printf("%d %d",f,s);         }         else if((s-a)<(a-f))         {             System.out.printf("%d",s);         }         else         {             System.out.println(f);         }     } }

---
+++ nearest_fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Codemind{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        int f=0,s=1,m;
+        while(s<a)
+        {
+            m=f+s;
+            f=s;
+            s=m;
+        }
+        if((s-a)==(a-f))
+        {
+            System.out.printf("%d %d",f,s);
+        }
+        else if((s-a)<(a-f))
+        {
+            System.out.printf("%d",s);
+        }
+        else
+        {
+            System.out.println(f);
+        }
+    }
+}
