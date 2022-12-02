a5ac1822a1ccd5c111dea62387d947ad7e0a6fa2
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-12-02 14:27:25 +0530

    import java.util.*; class Codmind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         a=a.toLowerCase();         System.out.println(a);         sc.close();     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codmind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        a=a.toLowerCase();
+        System.out.println(a);
+        sc.close();
+    }
+}
